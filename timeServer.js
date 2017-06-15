05d4a222c4a9d88d8aee64829913f361a5d8e3fa
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-06-15 21:27:29 +0530

    Add solution for time server, excercise 10

---
+++ timeServer.js
@@ -0,0 +1,35 @@
+// Write a TCP time server!
+//
+// Your server should listen to TCP connections on the port provided by the
+// first argument to your program. For each connection you must write the
+// current date & 24 hour time in the format:
+//
+//    "YYYY-MM-DD hh:mm"
+//
+// followed by a newline character. Month, day, hour and minute must be
+// zero-filled to 2 integers. For example:
+//
+//    "2013-07-06 17:42"
+//
+// After sending the string, close the connection.
+
+const net = require('net')
+
+const zeroFill = (i) => {
+  return (i < 10 ? '0' : '') + i
+}
+
+const now = () => {
+  var d = new Date()
+  return d.getFullYear() + '-' +
+  zeroFill(d.getMonth() + 1) + '-' +
+  zeroFill(d.getDate()) + ' ' +
+  zeroFill(d.getHours()) + ':' +
+  zeroFill(d.getMinutes())
+}
+
+const server = net.createServer((socket) => {
+  socket.end(now() + '\n')
+})
+
+server.listen(process.argv[2])
